ad9f298bfdd72e833d0e2e5b987b95b02fa7d1ed
Bryan Javier <1878bj@gmail.com>
2022-05-22 14:39:56 -0400

    Añadido listado de frases

---
+++ phrase_list.py
@@ -0,0 +1,8 @@
+Phrases = [
+    ['Cuanto más grande es la dificultad, más gloria hay en superarla','Epícuro'],
+    ['La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante','Søren Kierkegaard'],
+    ['El hombre valiente es el que no solo supera a sus enemigos, sino también a sus placeres','Demócrito'],
+    ['La creatividad requiere que la valentía se desprenda de las certezas','Erich Fromm'],
+    ['Aquellos que no conocen la historia están condenados a repetirla','Edmund Burke']
+]
+
