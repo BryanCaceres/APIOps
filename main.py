a8cff32658e6cd338562db207bdd860e020fc35f
Bryan Javier <1878bj@gmail.com>
2022-05-22 16:44:36 -0400

    hot fix

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from fastapi import FastAPI
 app =  FastAPI()
 
 
-@APIOps.get('/')
+@app.get('/')
 def PhraseGenerator():
+    Phrases = [
+
+    ['Cuanto más grande es la dificultad, más gloria hay en superarla','Epícuro'],
+    ['La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante','Søren Kierkegaard'],
+    ['El hombre valiente es el que no solo supera a sus enemigos, sino también a sus placeres','Demócrito'],
+    ['La creatividad requiere que la valentía se desprenda de las certezas','Erich Fromm'],
+    ['Aquellos que no conocen la historia están condenados a repetirla','Edmund Burke']
+    ]
     n = random.randint(0,len(Phrases)-1)
 
     
