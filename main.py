5dde6f1ddc851c446954639e60e722de33adc81b
Bryan Javier <1878bj@gmail.com>
2022-05-23 01:02:39 -0400

    new phrases in the list

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import random
 
 from phrases_list import Phrases
+
 from fastapi import FastAPI
 
 app =  FastAPI()
 
 @app.get('/')
 def PhraseGenerator():
-    
+
     n = random.randint(0,len(Phrases)-1)
 
-    
-    return {
-    
-    'Phrase':Phrases[n][0],
-    'Author':Phrases[n][1]
-    
-    }
+    return { 'Phrase':Phrases[n][0],  'Author':Phrases[n][1]  }
--- phrases_list.py
+++ phrases_list.py
@@ -1,9 +1,28 @@
 Phrases = [
 
-    ['Cuanto más grande es la dificultad, más gloria hay en superarla','Epícuro'],
+    ['Cuanto más grande es la dificultad, más gloria hay en superarla','Epícuro']
     ['La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante','Søren Kierkegaard'],
     ['El hombre valiente es el que no solo supera a sus enemigos, sino también a sus placeres','Demócrito'],
     ['La creatividad requiere que la valentía se desprenda de las certezas','Erich Fromm'],
-    ['Aquellos que no conocen la historia están condenados a repetirla','Edmund Burke']
-    
+    ['Aquellos que no conocen la historia están condenados a repetirla','Edmund Burke'],
+    ['El hombre está condenado a ser libre','Jean-Paul Sartre'],
+    ['En todo hay una parte de todo','Anaxágoras'],
+    ['La mejor parte de la belleza es aquella que ninguna imagen puede expresar ','Francis Bacon'],
+    ['De una pequeña chispa puede prender una llama ','Dante'],
+    ['El entorno da forma a las acciones del individuo','B. F. Skinner'],
+    ['Nadie puede herirme sin mi permiso','Gandhi'],
+    ['Puedo controlar mis pasiones y emociones si puedo entender su naturaleza','Spinoza'],
+    ['os seres humanos no nacen para siempre el día en que sus madres los alumbran, sino que la vida los obliga a parirse a sí mismos una y otra vez','Gabriel García Márquez'],
+    ['Ser independiente de la opinión pública es la primera condición formal para lograr algo grande','Friedrich Hegel'],
+    ['Saber es relativamente fácil. Querer y obrar de acuerdo a lo que uno quisiera, es siempre más duro','Aldous Huxley'],
+    ['Donde funciona un televisor, seguro que hay alguien que no está leyendo','John Irving'],
+    ['La juventud es feliz porque tiene la capacidad de ver la belleza. Cualquiera que conserve la capacidad de ver la belleza jamás envejece','Franz Kafka'],
+    ['El tacto es el arte de dejar ver algo sin hacer un enemigo','Isaac Newton'],
+    ['El que es bueno para poner excusas rara vez es bueno para cualquier otra cosa','Benjamin Franklin'],
+    ['Aferrarse a la ira es como beber veneno y esperar que la otra persona muera','Buda'],
+    ['No hay nada como volver a un lugar que permanece sin cambiar para encontrar las formas en las que tu mismo has alterado','Nelson Mandela'],
+    ['Si no creemos en la libertad de expresión de las personas que despreciamos, no creemos en ella en absoluto ','Noam Chomsky'],
+    ['Lo que logramos internamente cambiará nuestra realidad exterior','Plutarco'],
+    ['No se desarrolla el coraje al ser feliz en tus relaciones todos los días. Se desarrolla el coraje al sobrevivir tiempos difíciles y desafiando la adversidad','Epícuro']
+
     ]
