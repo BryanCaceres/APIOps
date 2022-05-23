26688a8bbbc62cb5d12cbd3081b77261fbf0f7d5
Bryan Javier <1878bj@gmail.com>
2022-05-23 00:51:07 -0400

    adding the phrases list on a side doc

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import random
+
+from phrases_list import Phrases
 from fastapi import FastAPI
 
 app =  FastAPI()
 
 @app.get('/')
 def PhraseGenerator():
-    Phrases = [
-
-    ['Cuanto más grande es la dificultad, más gloria hay en superarla','Epícuro'],
-    ['La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante','Søren Kierkegaard'],
-    ['El hombre valiente es el que no solo supera a sus enemigos, sino también a sus placeres','Demócrito'],
-    ['La creatividad requiere que la valentía se desprenda de las certezas','Erich Fromm'],
-    ['Aquellos que no conocen la historia están condenados a repetirla','Edmund Burke']
-    ]
-
+    
     n = random.randint(0,len(Phrases)-1)
 
     
